b17627182d48ec78bbdd1e7f65274eb4fa174db9
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-12-20 12:10:37 +1000

    Routing For /feed and /me

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Feed from './views/Feed.vue';
+import Me from './views/Me.vue';
 
 Vue.use(Router);
 
@@ -10,16 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'feed',
+      component: Feed,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/me',
+      name: 'me',
+      component : Me
     },
   ],
 });
---
+++ src/views/Me.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="me">
+    Me
+    <HelloWorld msg="Me"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Me extends Vue {}
+</script>
