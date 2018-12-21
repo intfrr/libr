16b5292772c04609de9c051cf0b4e6dd1e962b8d
Robbie Cahill <insprintorob@users.noreply.github.com>
2018-12-21 23:19:50 +1000

    Header for /me

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    Me
-  </div>
+    <v-layout>
+      <v-flex xs12>
+        <v-img src="/img/blog-header.png">
+            <v-layout>
+                <span>
+                    <v-img class="blog-avatar" src="/img/blog-avatar.png"></v-img>
+                </span>
+            </v-layout>
+        </v-img>
+        <v-toolbar color="secondary" dark>
+            POSTS
+        </v-toolbar>
+      </v-flex>
+    </v-layout>
 </template>
 
 <script lang="ts">
@@ -10,3 +21,12 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({})
 export default class Me extends Vue {}
 </script>
+<style lang="scss">
+    .blog-avatar {
+        position: relative;
+        top: 256%;
+        left: 197%;
+        width: 80px;
+        height: 80px;
+    }
+</style>
