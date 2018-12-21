ec01950bc43df9a86f68e96c4b45a14c1ed3a30c
Robbie Cahill <robbieonline123 add the @ symbol here gmail.com>
2018-12-21 16:23:39 +1000

    Design for feed

---
+++ src/model/feed-service.ts
@@ -0,0 +1,24 @@
+const FeedService = {
+    // @todo: Plug this into a real backend
+    getFeed(): Array<any> {
+        return [
+            {
+                type: "IMAGE",
+                src: "https://i.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif",
+                tags: "star wars,cats,fighting,battle,lightsaber"
+            },
+            {
+                type: "IMAGE",
+                src: "https://i.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif",
+                tags: "star wars,cats,fighting,battle,lightsaber"
+            },
+            {
+                type: "IMAGE",
+                src: "https://i.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif",
+                tags: "star wars,cats,fighting,battle,lightsaber"
+            }
+        ];
+    }
+};
+
+export default FeedService;
--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,14 +1,54 @@
 <template>
-  <v-container>
-    <v-layout>
-      Feed
-    </v-layout>
-  </v-container>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-card-title primary-title>
+            <span>
+                <b>supercatgifs</b>
+                <v-btn small color="primary">Follow</v-btn>
+            </span>
+        </v-card-title>
+
+        <v-img
+          src="https://i.giphy.com/media/Ov5NiLVXT8JEc/giphy.gif"
+        ></v-img>
+
+        <v-card-title>
+          <div>
+            <h3 class="headline mb-0">Lightsabre cats</h3>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+            <v-btn flat icon color="pink">
+              <v-icon>favorite_outline</v-icon>
+            </v-btn>
+            <v-btn flat icon color="green">
+              <v-icon>cached</v-icon>
+            </v-btn>
+            <v-btn flat icon color="blue">
+              <v-icon>chat_bubble_outline</v-icon>
+            </v-btn>
+            <v-btn flat icon color="blue">
+              <v-icon>share</v-icon>
+            </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
+import FeedService from "@/model/feed-service";
 
 @Component({})
-export default class Feed extends Vue {}
+export default class Feed extends Vue {
+  feed: Array<any>;
+
+  constructor() {
+    super();
+    this.feed = FeedService.getFeed();
+  }
+}
 </script>
